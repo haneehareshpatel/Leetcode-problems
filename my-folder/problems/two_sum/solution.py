8dc91fa55ad868a191d9a78294c914ea712b8ae6
haneehareshpatel <136635089+haneehareshpatel@users.noreply.github.com>
2022-05-27 06:07:17 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,15 +1,6 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         for x in range(0,len(nums)):
-            for y in range(0,len(nums)):
-                if x!=y:
-                    sum = nums[x]+nums[y]
-                    if sum == target:
-                        return [x,y]
-                
+            for y in range(x+1,len(nums)):
+                if nums[x]+nums[y]==target:
+                    return [x,y]
