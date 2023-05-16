571745860df26a9c1c08cb171c26a2ec779d6d39
haneehareshpatel <136635089+haneehareshpatel@users.noreply.github.com>
2023-05-16 17:38:53 +0000

    Sync LeetCode submission - N-th Tribonacci Number (python3)

--- my-folder/problems/n-th_tribonacci_number/solution.py
+++ my-folder/problems/n-th_tribonacci_number/solution.py
@@ -1,15 +1,11 @@
-class Solution(object):
-    def tribonacci(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        dp=[0]*(n+1)
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp = [-1]*(n+1)
         dp[0]=0
-        if n>=1:
+        if n>0:
             dp[1]=1
-        if n>=2:
+        if n >1:
             dp[2]=1
-        for i in range(3, n+1):
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
         return dp[n]
