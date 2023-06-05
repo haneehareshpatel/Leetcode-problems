ac080f1753874202e63dd79ce2a9d32d54eb22aa
haneehareshpatel <136635089+haneehareshpatel@users.noreply.github.com>
2023-06-05 00:53:58 +0000

    Sync LeetCode submission - Generate Parentheses (python3)

--- my-folder/problems/generate_parentheses/solution.py
+++ my-folder/problems/generate_parentheses/solution.py
@@ -1,15 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        
-        def helper(left , right , ans):
-            if left:
-                helper(left-1, right, ans + '(')
-            if right>left:
-                helper(left, right-1, ans+')')
-            if not right:
-                res.append(ans)
-        helper(n,n,"")
-        return res
-            
-                
+        ans = "("
+        ans_list = []
+        open = 1
+        def rec(ans, open, close):
+            if len(ans) < 2*n:
+                if ans[-1] == '(':
+                    if open < n:
+                        rec(ans + '(', open+1,close)
+                    if close < n:
+                        rec(ans + ')', open, close+1)
+                if ans[-1] == ')':
+                    if open > close and close <n:
+                        rec(ans + ')', open, close+1)
+                    if open < n:
+                        rec(ans+'(', open+1, close)
+            else:
+                ans_list.append(ans)
+        rec("(",1,0)
+        return ans_list
